0a803ed0771d5054f0c25e904f332c558e1bdaca
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 11:18:32 +0200

    Added date formatter class

---
+++ Pod/Classes/CFKDateFormatter.h
@@ -0,0 +1,13 @@
+//
+//  CFKDateFormatter.h
+//  Pods
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFKDateFormatter : NSObject
+
+@end
---
+++ Pod/Classes/CFKDateFormatter.m
@@ -0,0 +1,13 @@
+//
+//  CFKDateFormatter.m
+//  Pods
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//
+//
+
+#import "CFKDateFormatter.h"
+
+@implementation CFKDateFormatter
+
+@end

7de6f97f72facf858722149eed2bbfdf8e45044f
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 13:17:14 +0200

    Phone number formatter is implemented

--- Pod/Classes/CFKPhoneNumberFormatter.h
+++ Pod/Classes/CFKPhoneNumberFormatter.h
@@ -8,6 +8,49 @@
 
 #import <Foundation/Foundation.h>
 
+#import "NBAsYouTypeFormatter.h"
+
 @interface CFKPhoneNumberFormatter : NSObject
+
+/**
+ Creates and returns a formatter to be used with text entries.
+ The formatter object returned from this method is not a singleton object.
+ Every time this method is called a new formatter object with the region code of the current device is created and returned.
+ 
+ @return A formatter object
+ */
++ (NBAsYouTypeFormatter *)asYouTypeFormatter;
+
+/**
+ Creates and returns an example phone number based on the current device's country code.
+ 
+ @return Example phone number string
+ */
++ (NSString *)examplePhoneNumber;
+
+/**
+ Checks the validity of a given phone number string.
+ 
+ @return BOOL value indicating the validity of a number
+ */
++ (BOOL)isValidPhoneNumber:(NSString *)phoneNumberString;
+
+/**
+ Formats the given string as +1 500-123 45 67 and returns it.
+ 
+ @param number Phone number string to be formatted
+ 
+ @return String formatted as +1 500-123 45 67
+ */
++ (NSString *)formattedNumberInInternationalFormatFromNumber:(NSString *)number;
+
+/**
+ Formats the given string as +15001234567 and returns it.
+ 
+ @param number Phone number string to be formatted
+ 
+ @return String formatted as +15001234567
+ */
++ (NSString *)formattedNumberInE164FormatFromNumber:(NSString *)number;
 
 @end
--- Pod/Classes/CFKPhoneNumberFormatter.m
+++ Pod/Classes/CFKPhoneNumberFormatter.m
@@ -8,6 +8,101 @@
 
 #import "CFKPhoneNumberFormatter.h"
 
+#import "NBPhoneNumberUtil.h"
+#import "NBPhoneNumber.h"
+
+@interface CFKPhoneNumberFormatter ()
+
+/**
+ This is the utility that formats the numbers.
+ */
+@property (nonatomic) NBPhoneNumberUtil *util;
+
+/**
+ This property contains the current locale country code of the device.
+ */
+@property (nonatomic) NSString *region;
+
+@end
+
 @implementation CFKPhoneNumberFormatter
+
+#pragma mark - Singleton
+
++ (CFKPhoneNumberFormatter *)sharedInstance {
+    static CFKPhoneNumberFormatter *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [CFKPhoneNumberFormatter new];
+        sharedInstance.util = [NBPhoneNumberUtil new];
+        
+        sharedInstance.region = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+    });
+    return sharedInstance;
+}
+
+#pragma mark - Public methods
+
++ (NBAsYouTypeFormatter *)asYouTypeFormatter {
+    return [[NBAsYouTypeFormatter alloc] initWithRegionCode:[self sharedInstance].region];
+}
+
++ (NSString *)examplePhoneNumber {
+    
+    NBPhoneNumberUtil *util = [self sharedInstance].util;
+    NSString *region = [self sharedInstance].region;
+    
+    NBPhoneNumber *phoneNumber = [util getExampleNumber:region error:nil];
+    
+    return [util format:phoneNumber numberFormat:NBEPhoneNumberFormatINTERNATIONAL error:nil];
+    
+}
+
++ (BOOL)isValidPhoneNumber:(NSString *)phoneNumberString {
+    
+    NBPhoneNumberUtil *util = [self sharedInstance].util;
+    NSString *region = [self sharedInstance].region;
+    
+    NSError *error = nil;
+    
+    NBPhoneNumber *phoneNumber = [util parse:phoneNumberString defaultRegion:region error:&error];
+    
+    if (error) {
+        return NO;
+    } else {
+        return [util isValidNumber:phoneNumber];
+    }
+    
+}
+
++ (NSString *)formattedNumberInInternationalFormatFromNumber:(NSString *)number {
+    return [[self sharedInstance] formattedNumberInFormat:NBEPhoneNumberFormatINTERNATIONAL fromNumber:number];
+}
+
++ (NSString *)formattedNumberInE164FormatFromNumber:(NSString *)number {
+    return [[self sharedInstance] formattedNumberInFormat:NBEPhoneNumberFormatE164 fromNumber:number];
+}
+
+#pragma mark - Private methods
+
+- (NSString *)formattedNumberInFormat:(NBEPhoneNumberFormat)format fromNumber:(NSString *)number {
+    
+    NSError *error = nil;
+    
+    NBPhoneNumber *phoneNumber = [self.util parse:number defaultRegion:self.region error:&error];
+    
+    if (error) {
+        NSLog(@"Error: %@", error.description);
+    } else {
+        if ([self.util isValidNumber:phoneNumber]) {
+            return [self.util format:phoneNumber numberFormat:format error:&error];
+        } else {
+            NSLog(@"Phone number: %@", phoneNumber.description);
+        }
+    }
+    
+    return nil;
+    
+}
 
 @end

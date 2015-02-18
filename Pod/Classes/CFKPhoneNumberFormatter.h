ab8fd8c87bc85410c40c90d09f40a47da48aafc5
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 12:50:42 +0200

    Added phone number formatter class

---
+++ Pod/Classes/CFKPhoneNumberFormatter.h
@@ -0,0 +1,13 @@
+//
+//  CFKPhoneNumberFormatter.h
+//  Pods
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFKPhoneNumberFormatter : NSObject
+
+@end
---
+++ Pod/Classes/CFKPhoneNumberFormatter.m
@@ -0,0 +1,13 @@
+//
+//  CFKPhoneNumberFormatter.m
+//  Pods
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//
+//
+
+#import "CFKPhoneNumberFormatter.h"
+
+@implementation CFKPhoneNumberFormatter
+
+@end

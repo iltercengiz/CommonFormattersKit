b15fb27a74e79ed8f23159a85800b9ede7f7e057
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 11:14:49 +0200

    Example app view controllers are implemented

--- Example/CommonFormattersKit/Controller/CFKPhoneNumberFormatterViewController.m
+++ Example/CommonFormattersKit/Controller/CFKPhoneNumberFormatterViewController.m
@@ -8,30 +8,25 @@
 
 #import "CFKPhoneNumberFormatterViewController.h"
 
-@interface CFKPhoneNumberFormatterViewController ()
+@interface CFKPhoneNumberFormatterViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation CFKPhoneNumberFormatterViewController
 
+#pragma mark - View life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Text field delegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

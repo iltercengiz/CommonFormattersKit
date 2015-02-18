5220934591ac3ec4de72d5511436a9f76aae5652
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 10:58:18 +0200

    Added view controller classes

---
+++ Example/CommonFormattersKit/Controller/CFKDateFormatterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CFKDateFormatterViewController.h
+//  CommonFormattersKit
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFKDateFormatterViewController : UIViewController
+
+@end
---
+++ Example/CommonFormattersKit/Controller/CFKPhoneNumberFormatterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CFKPhoneNumberFormatterViewController.h
+//  CommonFormattersKit
+//
+//  Created by Ilter Cengiz on 18/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFKPhoneNumberFormatterViewController : UIViewController
+
+@end

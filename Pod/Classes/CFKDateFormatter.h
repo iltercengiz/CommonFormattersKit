db9659e388b45e13e70ce955c671dd8bc5db46ae
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-18 12:35:18 +0200

    Date formatter is implemented

--- Pod/Classes/CFKDateFormatter.h
+++ Pod/Classes/CFKDateFormatter.h
@@ -8,6 +8,78 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Available date formats.
+ These can be used to parse both strings to dates and dates to strings.
+ Every enum has an example with a comment line, given the date to be Wednesday, 18 February, 2015, 20:15:30.
+ */
+typedef NS_ENUM(NSInteger, CFKDateFormat) {
+    
+    // Defaults to `NSDateFormatterMediumStyle`
+    // Also any non-valid format (such as Integer value of 256) will be considered as default.
+    CFKDateFormatDefault = 0,
+    
+    // 2015
+    CFKDateFormatyyyy = 1,
+    
+    // 2015-02-18
+    CFKDateFormatyyyyMMdd,
+    
+    // 2015-02-18 20:15
+    CFKDateFormatyyyyMMddHHmm,
+    
+    // 2015-02-18 20:15:30
+    CFKDateFormatyyyyMMddHHmmss,
+    
+    // February
+    CFKDateFormatMMMM,
+    
+    // February / 2015
+    CFKDateFormatMMMMyyyy,
+    
+    // 18
+    CFKDateFormatd,
+    
+    // 18.02
+    CFKDateFormatddMM,
+    
+    // 18 Wed
+    CFKDateFormatdEEEEE,
+    
+    // Wednesday
+    CFKDateFormatEEEE,
+    
+    // W 18
+    CFKDateFormatEdd,
+    
+    // Wednesday 18 Feb
+    CFKDateFormatEEEEddMMM,
+    
+    // 20:15
+    CFKDateFormatHHmm,
+    
+};
+
 @interface CFKDateFormatter : NSObject
+
+/**
+ Returns an `NSDate` object parsed from the given string and format.
+ 
+ @param string The string to be parsed
+ @param format Date format to be used in parsing the string
+ 
+ @return Date object parsed from the string in the given format
+ */
++ (NSDate *)dateFromString:(NSString *)string withFormat:(CFKDateFormat)format;
+
+/**
+ Returns an `NSString` object parsed from the given date and format.
+ 
+ @param date The date to be parsed
+ @param format Date format to be used in parsing the date
+ 
+ @return String object parsed from the date in the given format
+ */
++ (NSString *)stringFromDate:(NSDate *)date withFormat:(CFKDateFormat)format;
 
 @end
--- Pod/Classes/CFKDateFormatter.m
+++ Pod/Classes/CFKDateFormatter.m
@@ -8,6 +8,69 @@
 
 #import "CFKDateFormatter.h"
 
+@interface CFKDateFormatter ()
+
+/**
+ Date formatter object used to parse strings into date objects and vice-versa.
+ */
+@property (nonatomic) NSDateFormatter *dateFormatter;
+
+@end
+
 @implementation CFKDateFormatter
+
+#pragma mark - Singleton
+
++ (CFKDateFormatter *)sharedInstance {
+    static CFKDateFormatter *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [CFKDateFormatter new];
+        sharedInstance.dateFormatter = [NSDateFormatter new];
+    });
+    return sharedInstance;
+}
+
+#pragma mark - Public methods
+
++ (NSDate *)dateFromString:(NSString *)string withFormat:(CFKDateFormat)format {
+    [[self sharedInstance] setFormat:format];
+    return [[self sharedInstance].dateFormatter dateFromString:string];
+}
+
++ (NSString *)stringFromDate:(NSDate *)date withFormat:(CFKDateFormat)format {
+    [[self sharedInstance] setFormat:format];
+    return [[self sharedInstance].dateFormatter stringFromDate:date];
+}
+
+#pragma mark - Private methods
+
+- (void)setFormat:(CFKDateFormat)format {
+    
+    switch (format) {
+            
+        case CFKDateFormatyyyy: self.dateFormatter.dateFormat = @"yyyy"; break;
+        case CFKDateFormatyyyyMMdd: self.dateFormatter.dateFormat = @"yyyy-MM-dd"; break;
+        case CFKDateFormatyyyyMMddHHmm: self.dateFormatter.dateFormat = @"yyyy-MM-dd HH:mm"; break;
+        case CFKDateFormatyyyyMMddHHmmss: self.dateFormatter.dateFormat = @"yyyy-MM-dd HH:mm:ss"; break;
+            
+        case CFKDateFormatMMMM: self.dateFormatter.dateFormat = @"MMMM"; break;
+        case CFKDateFormatMMMMyyyy: self.dateFormatter.dateFormat = @"MMMM / yyyy"; break;
+            
+        case CFKDateFormatd: self.dateFormatter.dateFormat = @"d"; break;
+        case CFKDateFormatddMM: self.dateFormatter.dateFormat = @"dd.MM"; break;
+        case CFKDateFormatdEEEEE: self.dateFormatter.dateFormat = @"dEEEEE"; break;
+            
+        case CFKDateFormatEEEE: self.dateFormatter.dateFormat = @"EEEE"; break;
+        case CFKDateFormatEdd: self.dateFormatter.dateFormat = @"E dd"; break;
+        case CFKDateFormatEEEEddMMM: self.dateFormatter.dateFormat = @"EEEE dd MMM"; break;
+            
+        case CFKDateFormatHHmm: self.dateFormatter.dateFormat = @"HH:mm"; break;
+            
+        default: self.dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+            
+    }
+    
+}
 
 @end

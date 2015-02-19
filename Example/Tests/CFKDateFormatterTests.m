e0f4ef2e1788763c8b1a2f4d9d257ce1ff3ede3d
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-19 14:45:39 +0200

    Date formatter test cases

--- Example/Tests/CFKDateFormatterTests.m
+++ Example/Tests/CFKDateFormatterTests.m
@@ -6,35 +6,98 @@
 //  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import "CFKDateFormatter.h"
 
-@interface CFKDateFormatterTests : XCTestCase
+SpecBegin(CFKDateFormatter)
 
-@end
+describe(@"Strings from dates", ^{
+    
+    __block NSDate *date;
+    
+    beforeAll(^{
+        // Date: Wed, 18 Feb 2015 20:35:45 GMT
+        date = [NSDate dateWithTimeIntervalSince1970:1424291745];
+        
+        // Set time zone
+        [CFKDateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
+    });
+    
+    it(@"should give 2015", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatyyyy]).to.equal(@"2015");
+    });
+    
+    it(@"should give 2015-02-18", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatyyyyMMdd]).to.equal(@"2015-02-18");
+    });
+    
+    it(@"should give 2015-02-18 20:35", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatyyyyMMddHHmm]).to.equal(@"2015-02-18 20:35");
+    });
+    
+    it(@"should give 2015-02-18 20:35:45", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatyyyyMMddHHmmss]).to.equal(@"2015-02-18 20:35:45");
+    });
+    
+    it(@"should give February", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatMMMM]).to.equal(@"February");
+    });
+    
+    it(@"should give February / 2015", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatMMMMyyyy]).to.equal(@"February / 2015");
+    });
+    
+    it(@"should give 18", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatd]).to.equal(@"18");
+    });
+    
+    it(@"should give 18.02", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatddMM]).to.equal(@"18.02");
+    });
+    
+    it(@"should give 18W", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatdEEEEE]).to.equal(@"18W");
+    });
+    
+    it(@"should give Wednesday", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatEEEE]).to.equal(@"Wednesday");
+    });
+    
+    it(@"should give Wed 18", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatEdd]).to.equal(@"Wed 18");
+    });
+    
+    it(@"should give Wednesday 18 Feb", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatEEEEddMMM]).to.equal(@"Wednesday 18 Feb");
+    });
+    
+    it(@"should give 20:35", ^{
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatHHmm]).to.equal(@"20:35");
+    });
+    
+});
 
-@implementation CFKDateFormatterTests
+describe(@"Locale", ^{
+    
+    __block NSDate *date;
+    
+    beforeAll(^{
+        // Date: Wed, 18 Feb 2015 20:35:45 GMT
+        date = [NSDate dateWithTimeIntervalSince1970:1424291745];
+        
+        // Set time zone
+        [CFKDateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
+    });
+    
+    it(@"should give Wednesday 18 Feb", ^{
+        [CFKDateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"US"]];
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatEEEEddMMM]).to.equal(@"Wednesday 18 Feb");
+    });
+    
+    it(@"should give Çarşamba 18 Şub", ^{
+        [CFKDateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"TR"]];
+        expect([CFKDateFormatter stringFromDate:date withFormat:CFKDateFormatEEEEddMMM]).to.equal(@"Çarşamba 18 Şub");
+    });
+    
+});
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SpecEnd

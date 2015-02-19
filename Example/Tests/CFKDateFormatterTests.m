217fd9b12ceab7811ed994db48417bf1eaf09386
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-19 12:26:31 +0200

    Added test class 'CFKDateFormatterTests'

---
+++ Example/Tests/CFKDateFormatterTests.m
@@ -0,0 +1,40 @@
+//
+//  CFKDateFormatterTests.m
+//  CommonFormattersKit
+//
+//  Created by Ilter Cengiz on 19/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CFKDateFormatterTests : XCTestCase
+
+@end
+
+@implementation CFKDateFormatterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
